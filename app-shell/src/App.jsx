857866bc5ce279bf00fad2d49010c1f420121a0e
Phillip Bailey <ztron500@gmail.com>
2024-11-23 20:05:11 -0500

    update

---
+++ app-shell/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    
+
+    </>
+  )
+}
+
+export default App
---
+++ app-shell/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+    const activeStyle = ({ isActive }) => ({
+        backgroundColor: isActive ? 'blue' : '',
+        color: isActive ? 'white' : '',
+        padding: '0.5rem 1rem',
+        borderRadius: '0.25rem',
+        textDecoration: 'none'
+      })
+  return (
+    <>
+   <nav className='navbar'>
+      <div className='logo'>
+        <NavLink to='/' >
+          Logo
+        </NavLink>
+      </div>
+      <ul className='navlinks'>
+        <NavLink to='/' style={activeStyle}>
+          Home
+        </NavLink>
+        <NavLink to='/about' style={activeStyle}>
+          About
+        </NavLink>
+        <NavLink to='/contact' style={activeStyle}>
+          Contact
+        </NavLink>
+      </ul>
+    </nav>
+
+    </>
+  )
+}
